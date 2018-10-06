569424ff1e84338c16dd650a7790c94cdeb3aa6d
Muhammad Shahrom Ali <estineali@gmail.com>
2018-10-06 16:59:12 +0500

    Submission
    All test cases Passed

---
+++ extraLongFactorials.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    fact = 1
+    for i in range(n):
+        fact *= n
+        n -= 1
+    print(fact)
+    return fact
+
+if __name__ == '__main__':
+    n = int(input())
+
+    extraLongFactorials(n)
